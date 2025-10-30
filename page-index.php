60583fd298dd6edcc9620600c356e3b48c09f353
Your Name <you@example.com>
2025-10-31 00:29:04 +0700

    первая секция

--- page-index.php
+++ page-index.php
@@ -2,7 +2,17 @@
 get_header();
 ?>
 
-<section class="inspire padding-top-100">1</section>
+<section class="inspire padding-top-100">
+    <div class="container">
+        <div class="inspire__left-blok">
+            <h1>Inspire, Transform and Conquer: Journey to Personal Excellence
+            </h1>
+        </div>
+        <div class="inspire__right-blok">
+            <img src="<?= assets('images/man.png'); ?>" alt="man">
+        </div>
+    </div>
+</section>
 <section class="self-discovery padding-top-100">2</section>
 <section class="mindset padding-top-100">3</section>
 <section class="emotional padding-top-100">4</section>
