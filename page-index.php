5890323b7eb575416f10450714ea1b471fcb8c07
Your Name <you@example.com>
2025-11-07 00:23:53 +0700

    Первая секция, хедер, футер

--- page-index.php
+++ page-index.php
@@ -4,12 +4,23 @@ get_header();
 
 <section class="inspire padding-top-100">
     <div class="container">
-        <div class="inspire__left-blok">
-            <h1>Inspire, Transform and Conquer: Journey to Personal Excellence
-            </h1>
-        </div>
-        <div class="inspire__right-blok">
-            <img src="<?= assets('images/man.png'); ?>" alt="man">
+        <div class="row">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+                <div class="inspire__left-blok">
+                    <h1>Inspire, Transform and Conquer: Journey to Personal Excellence
+                    </h1>
+                    <p>In this captivating blog, embark on a transformative journey to unravel the mysteries of
+                        self-inspiration and empowerment.
+
+                    </p>
+                    <button type="button" class="btn btn-dark">Subscribe</button>
+                </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <div class="inspire__right-blok">
+                    <img src="<?= assets('images/man.png'); ?>" alt="man">
+                </div>
+            </div>
         </div>
     </div>
 </section>
