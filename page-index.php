ee7e927abda8e275fa61d94d37327c62ffc4da63
Your Name <you@example.com>
2025-12-13 09:34:09 +0700

    Секция subscribe

--- page-index.php
+++ page-index.php
@@ -31,7 +31,26 @@ get_header();
 <section class="emotional padding-top-100">4</section>
 <section class="work-life padding-top-100">5</section>
 <section class="latest padding-top-100">6</section>
-<section class="subscribe padding-top-100">Subscribe for More</section>
+<section class="subscribe padding-top-100 padding-bottom-100">
+    <div class="container">
+        <div class="row">
+            <!--Текстовая часть-->
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+                <div class="subscribe__left-blok">
+                    <h2>Subscribe for More
+                    </h2>
+                    <p>Subscribe to our newsletter and be the first to access exclusive content and expert insights.
+                    </p>
+                    <button type="button" class="btn btn-dark">Subscribe</button>
+                </div>
+            </div>
+            <!--Изобразительная часть-->
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <img src="<?= assets('images/minfruit.jpg'); ?>" alt="man">
+            </div>
+        </div>
+    </div>
+</section>
 
 
 <?php
